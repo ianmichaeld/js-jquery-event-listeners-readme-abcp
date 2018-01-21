35d5d8290be0ea9382373d0ef7b18b3d5131585f
Ian Dewsbury <ianmichaeld@gmail.com>
2018-01-21 18:10:20 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,10 +11,16 @@ function frameIt(){
     img.addClass('tasty');
   })
 }
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.');
+  })
+}
 $(document).ready(function(){
 
 // call functions here
 getIt();
 frameIt();
-
+submitIt();
 });
